5b7a04845803f0790dddbfd9c94ca9686d4d0499
rodrigosouza83 <rodrigocarlos1883@gmail.com>
2021-02-01 16:22:59 -0300

    Atualização curso básico de Ruby

---
+++ program_oo.rb
@@ -0,0 +1,25 @@
+#Em Ruby tudo é objeto, e não precisa usar return dentro de um método.
+
+class Carro # Estrutura de uma classe em Ruby, classe começa sempre em maiusculo
+	
+	attr_accessor :marca #Método que irá atribuir e ler um atributo 
+
+	def velocidade_maxima #Def, define um método dentro de uma classe, ou seja, velocidade_maxima é um método da classe Carro
+	  250
+	end
+	# def adiciona_marca(marca) #Aqui temos uma variável de instância com passagem de parâmetros
+	#   @marca = marca
+	# end
+	
+	# def marca #Método para acessar a variável marca de fora da classe Carro
+	#   @marca
+	# end
+end
+
+# Com método definido, precisaremos instanciar o objeto Carro
+
+carro = Carro.new #carro é uma variável do objeto Carro
+carro.marca = "Nissan"
+#carro.adiciona_marca("Nissan") # passagem de argumento "Nissan"
+puts carro.velocidade_maxima
+puts carro.marca
---
+++ soma.rb
@@ -0,0 +1,5 @@
+def somar(var1, var2)
+	calculo = var1 + var2
+end
+
+puts somar(8, 45)
